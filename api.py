a43010c1dbf7c09b22e3320cf3d69d10ebff6c64
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 16:10:50 +0700

    commit

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form
-import co
+import collect_links
 
 app = FastAPI(
     title="API for AI Market",
@@ -14,7 +14,5 @@ app = FastAPI(
 async def naver(label: str = Form(description='label text'),
                 page: int = Form(description='Page crawl (1 page about 80 image)')
                 ):
-    main.AutoCrawler((skip_already_exist=_skip, n_threads=_threads,
-                          do_flickr=_flickr, do_naver=_naver,do_pexels=_pexels, full_resolution=_full,
-                          face=_face, no_gui=_no_gui, limit=_limit, proxy_list=_proxy_list))
+    co
     
