88cf6ab23459d425cbe099c24d63db8f405ae32f
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 16:09:00 +0700

    commit

--- api.py
+++ api.py
@@ -14,30 +14,5 @@ app = FastAPI(
 async def naver(label: str = Form(description='label text'),
                 page: int = Form(description='Page crawl (1 page about 80 image)')
                 ):
-    url = "https://gateway.ipfs.airight.io/ipfs/"+input_source_hash
-    writer = PdfFileWriter()
-    remoteFile = urlopen(Request(url)).read()
-    memoryFile = io.BytesIO(remoteFile)
-    pdfFile = PdfFileReader(memoryFile)
-    for pageNum in range(pdfFile.getNumPages()):
-        currentPage = pdfFile.getPage(pageNum)
-        writer.addPage(currentPage)
-    fileNamePdf = secrets.token_hex(16)+'.pdf'
-    outputStream = open(fileNamePdf, "wb")
-    writer.write(fileNamePdf)
-    outputStream.close()
-    pdfFile = open(fileNamePdf, 'rb')
-    pdfreader = PyPDF2.PdfFileReader(pdfFile)
-    pageObj = ""
-    for page in pdfreader.pages:
-        pageObj += page.extractText() + "\n"
-    fileName = secrets.token_hex(16)+'.txt'
-    f = open(fileName, 'w')
-    f.write(pageObj)
-    f.close()
-    url = 'http://128.199.70.52:5001/api/v0/add'
-    files = {'file': open(fileName, 'rb')}
-    response = requests.post(url, files=files)
-    os.remove(fileName)
-    os.remove(fileNamePdf)
-    return response.json() 
+    
+    
