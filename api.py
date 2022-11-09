f1621aacfdecad89a5dce9370815aaebcb1c9c28
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 16:18:11 +0700

    commit

--- api.py
+++ api.py
@@ -14,6 +14,6 @@ app = FastAPI(
 async def naver(label: str = Form(description='label text'),
                 page: int = Form(description='Page crawl (1 page about 80 image)')
                 ):
-    res = CollectLinks.naver('dog',add_url='&face=1')
+    res = CollectLinks.naver(keyword='dog',add_url='&face=1')
     print(res)
     
