8fd787a1a74e8d40662f3d8bf5f3726b6593aed4
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 15:08:38 +0700

    commit

--- pexels.py
+++ pexels.py
@@ -18,6 +18,8 @@ Copyright 2022 hoangks5
 # Import API class from pexels_api package
 from pexels_api import API
 # Type your Pexels API
+from dotenv import load_dotenv
+load_dotenv()
 PEXELS_API_KEY = '563492ad6f91700001000001c6decedb146c4053a03e0b07bee17e8b'
 # Create API object
 api = API(PEXELS_API_KEY)
