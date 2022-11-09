128bfdcf1369cf2582e3778e3e7c27ea3f28bfcd
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 17:14:12 +0700

    commit

--- api.py
+++ api.py
@@ -34,7 +34,7 @@ async def flickr(label: str = Form(description='label text'),
                  page: int = Form(description='number page')
                 ):
     collect = CollectLinks(no_gui=True, proxy=False)
-    links = collect.flickr(keyword=label,add_url='')
+    links = collect.flickr(keyword=label,page=page,add_url='')
     res = {
         'data': links
     }
