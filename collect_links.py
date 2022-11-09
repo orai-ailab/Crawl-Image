1a0db4ef1477bcddcf3cf46255f9c9a6c25a039f
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 11:44:04 +0700

    commit

--- collect_links.py
+++ collect_links.py
@@ -209,8 +209,8 @@ class CollectLinks:
             elem.send_keys(Keys.PAGE_DOWN)
             time.sleep(0.2)
 
-        imgs = self.browser.find_elements(By.CLASS_NAME,
-                                          'photo-list-photo-container')
+        imgs = self.browser.find_elements(By.TAG_NAME,
+                                          'img')
 
         print('Scraping links')
 
