b273ed8e088dd8a75b15ada59120c765ea37dcd4
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-11-09 16:06:10 +0700

    commit

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="API for AI Market",
+    description="",
+    version="1.0",
+    docs_url='/docs',
+    openapi_url='/openapi.json', # This line solved my issue, in my case it was a lambda function
+    redoc_url='/redoc'
+)
